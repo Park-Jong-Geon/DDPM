c2515edf6146330e74e8944d40ff8ccc0ac6d928
Park-Jong-Geon <panyaang99@gmail.com>
2023-03-05 12:26:01 +0000

    Minor changes

--- graph.py
+++ graph.py
@@ -1,8 +1,8 @@
 import os
 import matplotlib.pyplot as plt
 
-text_name = 'train_log.txt'
-# text_name = 'test.txt'
+# text_name = 'train_log.txt'
+text_name = 'test.txt'
 
 f = open(text_name)
 lines = f.readlines()
@@ -22,5 +22,5 @@ for line in lines:
 partition = text_name.split('.')
 
 plt.plot(loss)
-plt.axis((0, 1000, 0.025, 0.03))
+plt.axis((0, 200, 0.025, 0.03))
 plt.savefig(f"{partition[0]}_loss_curve.png")
