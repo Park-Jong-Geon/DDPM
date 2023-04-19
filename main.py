d814a6aeb3eb3b64f07348098789d7779706451e
Park-Jong-Geon <panyaang99@gmail.com>
2023-04-19 05:59:41 +0000

    TTUR official FID, minor configuration changes

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         ckpt_ema = None
         if _DDPM.ckpt_dir != None:
                 ckpt, ckpt_ema = _DDPM.load_ckpt()
-        
+        '''
         ds = _DDPM.load_dataset(_DDPM.dataset, _DDPM.batch)
         
         _DDPM.train(ds, ckpt, ckpt_ema)
@@ -23,5 +23,6 @@ if __name__ == "__main__":
         trained_ckpt, trained_ckpt_ema = _DDPM.load_ckpt()
         
         _DDPM.sample(trained_ckpt_ema, 50000)
-        
+        '''
+        _DDPM.sample(ckpt_ema, 50000)
         print(_DDPM.fid.calculate_fid('fid_stats_cifar10_train.npz', _DDPM.sample_dir))
